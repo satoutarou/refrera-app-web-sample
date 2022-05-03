019ad2ee1368da78ca0aa028bb80d3207ebc4ee8
chiba <>
2022-05-03 19:43:13 +0900

    edit pwa

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,8 @@
+importScripts('https://www.gstatic.com/firebasejs/9.6.10/firebase-app.js')
+importScripts('https://www.gstatic.com/firebasejs/9.6.10/firebase-messaging.js')
+
+firebase.initializeApp({
+    apikey:"AIzaSyBo7OkVYPbVgjGuEv3I7Bn9jdnr4b2z2i4",
+    projectId:"refrela-app",
+    messagingSenderId:"608200374244",
+})
